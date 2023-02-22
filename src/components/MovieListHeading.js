f11fc76744ba6cfa5afc14b16d95fae75e68a407
tejas <tejasbhat6@gmail.com>
2023-02-22 08:36:12 +0530

    Second commit

--- src/components/MovieListHeading.js
+++ src/components/MovieListHeading.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const MovieListHeading = (props) => {
   return (
+    // Display titles of the movies
     <div className='col'>
         <h1>{props.heading}</h1>
     </div>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const SearchBox = (props) => {
   return (
+    // Search box
     <div className='col col-sm-4'>
         <input className='form-control' placeholder='Type to search' value={props.value} onChange={(event)=>{
             props.setSearchValue(event.target.value);
